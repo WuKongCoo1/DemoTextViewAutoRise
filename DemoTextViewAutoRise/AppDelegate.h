802116d959a28eb619b8f9c4f162183e3bb24927
WuKongCoo1 <wukong@wukedemac-mini.local>
2016-08-18 10:20:54 +0800

    Initial Commit

---
+++ DemoTextViewAutoRise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoTextViewAutoRise
+//
+//  Created by 吴珂 on 16/8/18.
+//  Copyright © 2016年 吴珂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoTextViewAutoRise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoTextViewAutoRise
+//
+//  Created by 吴珂 on 16/8/18.
+//  Copyright © 2016年 吴珂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
